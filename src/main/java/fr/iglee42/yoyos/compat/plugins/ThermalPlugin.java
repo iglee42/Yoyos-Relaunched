2282cc3ca3515b05101a19a456d18b82d4518525
iglée42 <igleev2@gmail.com>
2025-03-05 16:30:44 +0100

    Effects on thermal yoyo

--- src/main/java/fr/iglee42/yoyos/compat/plugins/ThermalPlugin.java
+++ src/main/java/fr/iglee42/yoyos/compat/plugins/ThermalPlugin.java
@@ -1,18 +1,25 @@
 package fr.iglee42.yoyos.compat.plugins;
 
+import cofh.lib.util.Utils;
 import fr.iglee42.yoyos.common.YoyoItem;
 import fr.iglee42.yoyos.common.YoyoTier;
+import fr.iglee42.yoyos.common.init.YoyosEnchantments;
 import fr.iglee42.yoyos.compat.IYoyoPlugin;
 import fr.iglee42.yoyos.compat.YoyoPlugin;
 import fr.iglee42.yoyos.compat.YoyoPluginHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.fml.ModList;
 
 import java.util.UUID;
+
+import static cofh.core.init.CoreMobEffects.ENDERFERENCE;
 
 @YoyoPlugin
 public class ThermalPlugin implements IYoyoPlugin {
@@ -92,7 +99,33 @@ public class ThermalPlugin implements IYoyoPlugin {
                 .setWeight(4.5)
                 .setLength(12.0)
                 .setDuration(600)
-                .setDamage(7.0));
+                .setDamage(7.0)
+                .addEntityInteraction((yoyoStack, player, hand, yoyo, target) -> {
+                    if (!yoyo.canAttack() || !target.isAlive()) return false;
+                    if (!YoyoItem.isAttackEnable(yoyoStack)) return false;
+                    if (!ForgeHooks.onPlayerAttackTarget(player, target)) return false;
+
+                    UUID entityUUID = target.getUUID();
+
+                    if ((player.getShoulderEntityLeft().contains("UUID") && entityUUID.equals(player.getShoulderEntityLeft().getUUID("UUID"))) || (player.getShoulderEntityRight().contains("UUID") && entityUUID.equals(player.getShoulderEntityRight().getUUID("UUID"))))
+                        return false;
+
+                    if (target.isAttackable()){
+                        BlockPos randPos = yoyo.blockPosition().offset(-128 + yoyo.level().random.nextInt(257), yoyo.level().random.nextInt(8), -128 + yoyo.level().random.nextInt(257));
+
+                        if (!yoyo.level().getBlockState(randPos).isSolid()) {
+                            if (target instanceof LivingEntity) {
+                                if (Utils.teleportEntityTo(target, randPos)) {
+                                    ((LivingEntity) target).addEffect(new MobEffectInstance(ENDERFERENCE.get(), 40, 0, false, false));
+                                }
+                            } else if (target.level().getGameTime() % 40 == 0) {
+                                target.setPos(randPos.getX(), randPos.getY(), randPos.getZ());
+                                target.playSound(SoundEvents.ENDERMAN_TELEPORT, 1.0F, 1.0F);
+                            }
+                        }
+                    }
+                    return true;
+                }));
 
 
         if (ModList.get().isLoaded("thermal_integration")){
