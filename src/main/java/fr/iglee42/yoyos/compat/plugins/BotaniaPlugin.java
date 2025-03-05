98926d1e5b5a87443ad6052a314169ccad69c4e2
iglée42 <igleev2@gmail.com>
2025-03-05 16:31:58 +0100

    Fix gaia yoyo texture

--- src/main/java/fr/iglee42/yoyos/compat/plugins/BotaniaPlugin.java
+++ src/main/java/fr/iglee42/yoyos/compat/plugins/BotaniaPlugin.java
@@ -10,12 +10,18 @@ import fr.iglee42.yoyos.compat.botania.ElementiumYoyoItem;
 import fr.iglee42.yoyos.compat.botania.GaiaYoyoItem;
 import fr.iglee42.yoyos.compat.botania.ManaYoyoItem;
 import fr.iglee42.yoyos.compat.botania.TerrasteelYoyoItem;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Tiers;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegisterEvent;
+import vazkii.botania.client.core.handler.ClientTickHandler;
+import vazkii.botania.common.item.BotaniaItems;
 import vazkii.botania.common.lib.BotaniaTags;
 
 import java.util.ArrayList;
@@ -35,6 +41,7 @@ public class BotaniaPlugin implements IYoyoPlugin {
                     .setDuration(400)
                     .setDamage(5.5)
                     .setCustomCord("mana")
+                    .setCustomStick("botania:livingwood_twig")
                     .setCustomConstructor(ManaYoyoItem.class.getConstructor(YoyoTier.class)));
             helper.registerYoyo(new YoyoTier("elementium", Tiers.DIAMOND, modId())
                     .setWeight(5.0)
@@ -42,6 +49,7 @@ public class BotaniaPlugin implements IYoyoPlugin {
                     .setDuration(500)
                     .setDamage(6.0)
                     .setCustomCord("mana")
+                    .setCustomStick("botania:dreamwood_twig")
                     .setCustomConstructor(ElementiumYoyoItem.class.getConstructor(YoyoTier.class)));
             helper.registerYoyo(new YoyoTier("terrasteel", Tiers.NETHERITE, modId())
                     .setWeight(6.0)
@@ -49,6 +57,7 @@ public class BotaniaPlugin implements IYoyoPlugin {
                     .setDuration(650)
                     .setDamage(7.0)
                     .setCustomCord("mana")
+                    .setCustomStick("botania:livingwood_twig")
                     .setCustomConstructor(TerrasteelYoyoItem.class.getConstructor(YoyoTier.class)));
             helper.registerYoyo(new YoyoTier("gaia_spirit", Tiers.NETHERITE, modId())
                     .setWeight(5.0)
@@ -56,8 +65,17 @@ public class BotaniaPlugin implements IYoyoPlugin {
                     .setDuration(700)
                     .setDamage(8.0)
                     .setCustomCord("mana")
+                    .setCustomStick("botania:livingwood_twig")
                     .setCustomConstructor(GaiaYoyoItem.class.getConstructor(YoyoTier.class))
-                    .setCustomItem("gaia_ingot"));
+                    .setCustomItem("gaia_ingot")
+                    .setCustomModel("""
+                            {
+                               "parent": "item/handheld",
+                               "textures": {
+                                    "layer0": "yoyos:item/botania/manasteel",
+                                    "layer1": "yoyos:item/botania/gaia_spirit"
+                                }
+                            }"""));
 
             if (ModList.get().isLoaded("mythicbotany")){
                 helper.registerYoyo(new YoyoTier("alfsteel",Tiers.NETHERITE,modId())
@@ -67,7 +85,23 @@ public class BotaniaPlugin implements IYoyoPlugin {
                         .setDamage(9.0)
                         .setMod("mythicbotany")
                         .setCustomCord("mana")
-                        .setCustomConstructor(TerrasteelYoyoItem.class.getConstructor(YoyoTier.class)));
+                        .setCustomConstructor(TerrasteelYoyoItem.class.getConstructor(YoyoTier.class))
+                        .setCustomRecipe("""
+                                {
+                                  "type": "minecraft:smithing_transform",
+                                  "addition": {
+                                    "item": "mythicbotany:alfsteel_ingot"
+                                  },
+                                  "base": {
+                                    "item": "yoyos:terrasteel_yoyo"
+                                  },
+                                  "result": {
+                                    "item": "yoyos:alfsteel_yoyo"
+                                  },
+                                  "template": {
+                                    "item": "mythicbotany:alfsteel_template"
+                                  }
+                                }"""));
             }
 
         }catch (NoSuchMethodException ignored){}
@@ -90,5 +124,10 @@ public class BotaniaPlugin implements IYoyoPlugin {
         if (ModList.get().isLoaded("mythicbotany")) manaItems.add("yoyos:alfsteel_yoyo");
         tags.put(BotaniaTags.Items.MANA_USING_ITEMS.location(),manaItems);
         return tags;
+    }
+
+    @Override
+    public void clientSetup(FMLClientSetupEvent event) {
+        Minecraft.getInstance().getItemColors().register((s,t)->t == 1 ? Mth.hsvToRgb(ClientTickHandler.ticksInGame * 2 % 360 / 360F, 0.25F, 1F) : -1, ForgeRegistries.ITEMS.getValue(new ResourceLocation("yoyos:gaia_spirit_yoyo")));
     }
 }
